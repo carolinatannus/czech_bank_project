6d817d60d1d0d7b8721bc3b53ec6ec77bc671453
tannus1 <102600594+tannus1@users.noreply.github.com>
2022-08-30 10:55:12 -0300

    conclude possible correlation between district variables and debt ratio

--- czech_bank_project.sql
+++ czech_bank_project.sql
@@ -106,18 +106,18 @@ SELECT d.A2 AS district_name
     , COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) / COUNT(l.account_id) AS debt_ratio
     , AVG((COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) / COUNT(l.account_id))) OVER(PARTITION BY d.A3) AS avg_region_debt_ratio
     , d.A11 AS avg_salary
-    , ROUND((d.A12 + d.A13)/2, 2) AS avg_unemployment_rate
+    , ROUND(AVG(d.A12 + d.A13), 2) AS avg_unemployment_rate
     , d.A14 AS entrepreneur_per_1000
-    , ROUND((d.A15 + d.A16)/2) AS avg_crimes
+    , ROUND(AVG(d.A15 + d.A16)/2) AS avg_crimes
 FROM loan l
 JOIN account a
 ON a.account_id = l.account_id
 JOIN district d
 ON d.A1 = a.district_id
-GROUP BY 1, 2, 3, 8, 9, 10, 11
+GROUP BY 1, 2, 3, 8, 10
 HAVING unpaid_loans > 2;
 
--- Could there be a relationship between detb ratio and avg_salary?
+-- Could there be a relationship between debt ratio and avg_salary?
 SELECT d.A2 AS district_name
 	, d.A3 AS district_region
     , COUNT(l.account_id) AS current_loans
@@ -125,6 +125,7 @@ SELECT d.A2 AS district_name
     , COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) / COUNT(l.account_id) AS debt_ratio
     , AVG((COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) / COUNT(l.account_id))) OVER(PARTITION BY d.A3) AS avg_region_debt_ratio
     , d.A11 AS avg_salary
+    , (d.A11 / (COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) / COUNT(l.account_id))) AS avgsalary_debt_ratio
 FROM loan l
 JOIN account a
 ON a.account_id = l.account_id
@@ -133,23 +134,45 @@ ON d.A1 = a.district_id
 GROUP BY 1, 2, 7
 HAVING unpaid_loans > 2
 ORDER BY avg_salary;
+-- At first there does not seem to be a relation between these variables
 
--- What about debt ratio and unemployment rate?
+-- What about debt ratio and unemployment rate in 95'?
 SELECT d.A2 AS district_name
 	, d.A3 AS district_region
     , COUNT(l.account_id) AS current_loans
     , COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) AS unpaid_loans
     , COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) / COUNT(l.account_id) AS debt_ratio
     , AVG((COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) / COUNT(l.account_id))) OVER(PARTITION BY d.A3) AS avg_region_debt_ratio
-    , ROUND((d.A12 + d.A13)/2, 2) AS avg_unemployment_rate
+    , ROUND(AVG(d.A12), 2) AS unemployment_rate_95
+	, (ROUND(AVG(d.A12), 2) / (COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) / COUNT(l.account_id))) AS unemp95_debt_rate
 FROM loan l
 JOIN account a
 ON a.account_id = l.account_id
 JOIN district d
 ON d.A1 = a.district_id
-GROUP BY 1, 2, 7
+GROUP BY 1, 2
 HAVING unpaid_loans > 2
-ORDER BY avg_unemployment_rate;
+ORDER BY unemployment_rate_95;
+-- At first there does not seem to be a relation between these variables
+
+-- What about debt ratio and unemployment rate in 96'?
+SELECT d.A2 AS district_name
+	, d.A3 AS district_region
+    , COUNT(l.account_id) AS current_loans
+    , COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) AS unpaid_loans
+    , COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) / COUNT(l.account_id) AS debt_ratio
+    , AVG((COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) / COUNT(l.account_id))) OVER(PARTITION BY d.A3) AS avg_region_debt_ratio
+    , ROUND(AVG(d.A13), 2) AS unemployment_rate_96
+	, (ROUND(AVG(d.A13), 2) / (COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) / COUNT(l.account_id))) AS unemp96_debt_rate
+FROM loan l
+JOIN account a
+ON a.account_id = l.account_id
+JOIN district d
+ON d.A1 = a.district_id
+GROUP BY 1, 2
+HAVING unpaid_loans > 2
+ORDER BY unemployment_rate_96;
+-- At first there does not seem to be a relation between these variables
 
 -- Maybe debt ratio and entrepreneur per 1000?
 SELECT d.A2 AS district_name
@@ -159,6 +182,7 @@ SELECT d.A2 AS district_name
     , COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) / COUNT(l.account_id) AS debt_ratio
     , AVG((COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) / COUNT(l.account_id))) OVER(PARTITION BY d.A3) AS avg_region_debt_ratio
     , d.A14 AS entrepreneur_per_1000
+    , (d.A14 / COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) / COUNT(l.account_id)) AS entrepreneur_debt_ratio
 FROM loan l
 JOIN account a
 ON a.account_id = l.account_id
@@ -167,6 +191,7 @@ ON d.A1 = a.district_id
 GROUP BY 1, 2, 7
 HAVING unpaid_loans > 2
 ORDER BY entrepreneur_per_1000;
+-- At first there does not seem to be a relation between these variables
 
 -- Or debt ratio and total crimes?
 
@@ -176,12 +201,128 @@ SELECT d.A2 AS district_name
     , COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) AS unpaid_loans
     , COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) / COUNT(l.account_id) AS debt_ratio
     , AVG((COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) / COUNT(l.account_id))) OVER(PARTITION BY d.A3) AS avg_region_debt_ratio
-    , ROUND((d.A15 + d.A16)/2) AS avg_crimes
+    , ROUND(AVG(d.A15 + d.A16)/2) AS avg_crimes
+    , (ROUND(AVG(d.A15 + d.A16)/2) / COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) / COUNT(l.account_id)) AS crimes_debt_ratio
 FROM loan l
 JOIN account a
 ON a.account_id = l.account_id
 JOIN district d
 ON d.A1 = a.district_id
-GROUP BY 1, 2, 7
+GROUP BY 1, 2
 HAVING unpaid_loans > 2
 ORDER BY avg_crimes;
+-- At first there does not seem to be a relation between these variables
+
+-- Could there be any relationship between the number of municipalities of different sizes and the debt ratio?
+
+SELECT d.A2 AS district_name
+	, d.A3 AS district_region
+    , d.A4 AS inhabitants
+	, d.A5 AS number_mun_less_499
+    , d.A6 AS number_mun_bet500_1999
+    , d.A7 AS number_mun_bet2000_9999
+    , d.A8 AS number_mun_more_10000
+    , COUNT(l.account_id) AS current_loans
+    , COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) AS unpaid_loans
+    , COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) / COUNT(l.account_id) AS debt_ratio
+    , AVG((COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) / COUNT(l.account_id))) OVER(PARTITION BY d.A3) AS avg_region_debt_ratio
+FROM loan l
+JOIN account a
+ON a.account_id = l.account_id
+JOIN district d
+ON d.A1 = a.district_id
+GROUP BY 1, 2, 3, 4, 5, 6, 7
+HAVING unpaid_loans > 2;
+
+-- Municipalities with under 499 inhabitants?
+
+SELECT d.A2 AS district_name
+	, d.A3 AS district_region
+    , d.A4 AS inhabitants
+	, d.A5 AS number_mun_less_499
+    , (d.A5 / COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) / COUNT(l.account_id)) AS mun1_debt_ratio
+    , COUNT(l.account_id) AS current_loans
+    , COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) AS unpaid_loans
+    , COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) / COUNT(l.account_id) AS debt_ratio
+    , AVG((COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) / COUNT(l.account_id))) OVER(PARTITION BY d.A3) AS avg_region_debt_ratio
+FROM loan l
+JOIN account a
+ON a.account_id = l.account_id
+JOIN district d
+ON d.A1 = a.district_id
+GROUP BY 1, 2, 3, 4
+HAVING unpaid_loans > 2 AND number_mun_less_499 > 0;
+
+-- Municipalities with between 500 and 1999 inhabitants?
+
+SELECT d.A2 AS district_name
+	, d.A3 AS district_region
+    , d.A4 AS inhabitants
+    , d.A6 AS number_mun_bet500_1999
+    , (d.A6 / COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) / COUNT(l.account_id)) AS mun2_debt_ratio
+    , COUNT(l.account_id) AS current_loans
+    , COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) AS unpaid_loans
+    , COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) / COUNT(l.account_id) AS debt_ratio
+    , AVG((COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) / COUNT(l.account_id))) OVER(PARTITION BY d.A3) AS avg_region_debt_ratio
+FROM loan l
+JOIN account a
+ON a.account_id = l.account_id
+JOIN district d
+ON d.A1 = a.district_id
+GROUP BY 1, 2, 3, 4
+HAVING unpaid_loans > 2 AND number_mun_bet500_1999 > 0;
+
+-- Municipalities between 2000 and 9999 inhabitants?
+
+SELECT d.A2 AS district_name
+	, d.A3 AS district_region
+    , d.A4 AS inhabitants
+    , d.A7 AS number_mun_bet2000_9999
+    , (d.A7 / COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) / COUNT(l.account_id)) AS mun3_debt_ratio
+    , COUNT(l.account_id) AS current_loans
+    , COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) AS unpaid_loans
+    , COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) / COUNT(l.account_id) AS debt_ratio
+    , AVG((COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) / COUNT(l.account_id))) OVER(PARTITION BY d.A3) AS avg_region_debt_ratio
+FROM loan l
+JOIN account a
+ON a.account_id = l.account_id
+JOIN district d
+ON d.A1 = a.district_id
+GROUP BY 1, 2, 3, 4
+HAVING unpaid_loans > 2 AND number_mun_bet2000_9999 > 0;
+
+-- Municipalities with more than 10000 inhabitants?
+
+SELECT d.A2 AS district_name
+	, d.A3 AS district_region
+    , d.A4 AS inhabitants
+    , d.A8 AS number_mun_more_10000
+    , (d.A8 / COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) / COUNT(l.account_id)) AS mun4_debt_ratio
+    , COUNT(l.account_id) AS current_loans
+    , COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) AS unpaid_loans
+    , COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) / COUNT(l.account_id) AS debt_ratio
+    , AVG((COUNT(CASE WHEN l.status IN ('B', 'D') THEN 0 ELSE NULL END) / COUNT(l.account_id))) OVER(PARTITION BY d.A3) AS avg_region_debt_ratio
+FROM loan l
+JOIN account a
+ON a.account_id = l.account_id
+JOIN district d
+ON d.A1 = a.district_id
+GROUP BY 1, 2, 3, 4
+HAVING unpaid_loans > 2;
+
+/* Simply put, there does not seem to be a correlation between district variables (listed below) and the debt ratio of each district.
+
+District variables:
+- number of municipalities with under 499 inhabitants
+- number of municipalities between 500 and 1999 inhabitants
+- number of municipalities between 2000 and 9999 inhabitants
+- number of municipalities with over 10000 inhabitants
+- average salary
+- unemployment rate '95
+- unemployment rate '96
+- number of entrepreneurs per 1000 inhabitants
+- number of commited crimes
+
+Nevertheless, Bruntal and Opava both have a debt ratio bigger than the rest of their region.
+*/
+
